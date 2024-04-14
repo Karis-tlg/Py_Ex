52849b0fbc62cdc076bbdce285dea6106c68f31f
Karis-tlg <td.kha.18@gmail.com>
2024-04-14 23:35:29 +0700

    Update from push_to_github.bat

--- BT EX/XÂU CON ĐỐI XỨNG LỚN NHẤT.py
+++ BT EX/XÂU CON ĐỐI XỨNG LỚN NHẤT.py
@@ -1,10 +1,12 @@
-v = "AABAAOID"
+v = "AABAAOID"#8
 a = 0
 
 for i in range(len(v)):
     for j in range(i , len(v)):
         vr = v[i:j + 1]
+        #print(f"v[{i}:{j + 1}] = {v[i:j + 1]}")
         if vr == vr[::-1]:
+            print(f"v[{i}:{j + 1}] = {v[i:j + 1]}")
             if len(vr) > a:
                 a = len(vr)
             #a = max(a, len(vr))
