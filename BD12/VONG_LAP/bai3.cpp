9f3f91575883b3a1f580e43c6b30dd0ec862fac8
Karis-tlg <td.kha.18@gmail.com>
2024-10-20 23:11:39 +0700

    Update from push_to_github.bat

---
+++ BD12/VONG_LAP/bai3.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n, t = 1;
+    cin >> n;
+
+    for (int i = 1; i <= sqrt(n); i++) {
+        if (n % i == 0) {
+            t *= i;
+            if (i != n / i) t *= n / i;
+        }
+    } cout << t;
+}
+
